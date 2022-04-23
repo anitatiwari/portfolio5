e584e1a299414fb2152adc2abb5fe527939fed1e
Anita Tiwari <anit8621@stud.kea.dk>
2022-04-23 23:20:55 +0200

    changes

---
+++ my_portfolio/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.getElementById("menu").addEventListener("click", menuOnClick);;
+
+
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
